894b1694b27cc11eff7cd17fb1f9157daf3ed956
revanthkiran96 <116859708+revanthkiran96@users.noreply.github.com>
2022-12-28 17:37:56 +0530

    Update account.py

--- account.py
+++ account.py
@@ -7,4 +7,4 @@ class account:
 
 if __name__ == '__main__':
     accVerify = account()
-    print('The password length is ' + str(accVerify.check_password_length('offtoschool')))
+    print('The password length is ' + str(accVerify.check_password_length('offtoschol')))
